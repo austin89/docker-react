e78196312564beb8181ed0b17907489d882978e2
Austin Couey <rcouey@manh.com>
2020-01-29 16:28:34 -0500

    changed text in App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Du der ahdahts und dem?!
+          Ich bin ein Kodierer
         </p>
         <a
           className="App-link"
